33e49ee0ffa96819f2b04168008c5ff258049776
tariktunc <tarktunc@hotmail.com>
2023-08-24 16:08:15 +0300

    add buttonList and return {update}

--- app/components/calculator.js
+++ app/components/calculator.js
@@ -1,16 +1,124 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
-export default function calculator() {
-  const handleClick = (e) => {
-    console.log(e.target.value);
+export default function Home() {
+  const [value, setValue] = useState(0);
+  const [tempValue, setTempValue] = useState(0);
+  const [tempAction, setTempAction] = useState("");
+
+  const buttonList = [
+    {
+      text: "AC",
+      value: "AC",
+      isnum: false,
+    },
+    {
+      text: "/",
+      value: "/",
+      isnum: false,
+    },
+    {
+      text: "X",
+      value: "X",
+      isnum: false,
+    },
+    {
+      text: "7",
+      value: "7",
+      isnum: true,
+    },
+    {
+      text: "8",
+      value: "8",
+      isnum: true,
+    },
+    {
+      text: "9",
+      value: "9",
+      isnum: true,
+    },
+    {
+      isnum: false,
+      text: "-",
+      value: "-",
+    },
+    {
+      text: "4",
+      value: "4",
+      isnum: true,
+    },
+    {
+      text: "5",
+      value: "5",
+      isnum: true,
+    },
+    {
+      text: "6",
+      value: "6",
+      isnum: true,
+    },
+    {
+      text: "+",
+      value: "+",
+      isnum: false,
+    },
+    {
+      text: "1",
+      value: "1",
+      isnum: true,
+    },
+    {
+      text: "2",
+      value: "2",
+      isnum: true,
+    },
+    {
+      text: "3",
+      value: "3",
+      isnum: true,
+    },
+
+    {
+      text: "0",
+      value: "0",
+      isnum: true,
+    },
+    {
+      text: ".",
+      value: ".",
+      isnum: true,
+    },
+    {
+      text: "=",
+      value: "=",
+      isnum: false,
+    },
+  ];
+
+  const actions = (item) => {
+    switch (item.value) {
+      case "X":
+        setTempAction("X");
+        break;
+    }
   };
+
+  const clickedButton = (item) => {
+    if (item.isnum === true) {
+      setValue(value + item.value);
+    } else {
+      console.log(actions(item.value));
+      setValue(0);
+    }
+  };
+
   return (
     <div className="p-2 bg-black w-[340px] relative  border-2 border-solid border-47476b">
       {/* Formula Screen */}
       <div
         className=" font text-sm text-orange-600 text-right align-text-top leading-5 break-words"
         style={{ fontFamily: "digital" }}>
-        0
+        {tempValue}
       </div>
 
       {/* Output Screen */}
@@ -18,67 +126,24 @@ export default function calculator() {
         className="outputScreen text-2xl text-white text-right leading-9"
         id="display"
         style={{ fontFamily: "digital" }}>
-        0
+        {value}
       </div>
 
       {/* Buttons Screen */}
-      <div>
-        <button id="clear" value="AC" className="button jumbo">
-          AC
-        </button>
-        <button id="divide" value="/" className="button">
-          /
-        </button>
-        <button id="multiply" value="X" className="button">
-          X
-        </button>
-        <button id="seven" value="7" className="button">
-          7
-        </button>
-        <button id="eight" value="8" className="button">
-          8
-        </button>
-        <button id="nine" value="9" className="button">
-          9
-        </button>
-        <button id="subtract" value="-" className="button">
-          -
-        </button>
-        <button id="four" value="4" className="button">
-          4
-        </button>
-        <button id="five" value="5" className="button">
-          5
-        </button>
-        <button id="six" value="6" className="button">
-          6
-        </button>
-        <button id="add" value="+" className="button">
-          +
-        </button>
-        <button id="one" value="1" className="button">
-          1
-        </button>
-        <button id="two" value="2" className="button">
-          2
-        </button>
-        <button id="three" value="3" className="button">
-          3
-        </button>
-        <button id="zero" value="0" className="button jumbo">
-          0
-        </button>
-        <button id="decimal" value="." className="button">
-          .
-        </button>
+      {buttonList.map((item, index) => (
         <button
-          id="equals"
-          value="="
-          className="button"
-          style={{ position: "absolute", height: "130px", bottom: "8px" }}>
-          =
+          onClick={() => {
+            clickedButton(item);
+          }}
+          key={index}
+          value={item.value}
+          className={`button
+          ${item.text === "AC" ? "jumbo " : ""} 
+          ${item.text === "=" ? "vertical" : ""} 
+          ${item.text === "0" ? "zero" : ""}`}>
+          {item.text}
         </button>
-      </div>
+      ))}
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div id="app">
       <Calculator />
-      <div className="text-xl" style={{ fontFamily: "digital" }}>
+      <div className="text-xl text-center" style={{ fontFamily: "digital" }}>
         Designed and Coded by <br />
         Tarik
       </div>
